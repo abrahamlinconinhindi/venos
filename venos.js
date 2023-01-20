a81f8754ec77e5baf21b2d156b7a4eed7b75eee4
abrahamlinconinhindi <115974623+abrahamlinconinhindi@users.noreply.github.com>
2023-01-20 11:46:40 +0100

    js added

---
+++ venos.js
@@ -0,0 +1,9 @@
+document.querySelector("header").style.backgroundColor = "maroon";
+document.querySelector("header").style.color = "white";
+document.querySelector("nav").style.backgroundColor = "darkred";
+document.querySelectorAll("nav a").forEach(function(el) {
+  el.style.color = "white";
+});
+document.querySelector("main").style.backgroundColor = "tomato";
+document.querySelector("footer").style.backgroundColor = "firebrick";
+document.querySelector("footer").style.color = "white";
